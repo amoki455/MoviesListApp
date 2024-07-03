02abcf9b9d190af8d4f4290d4e794b4b3bccc677
Ahmed Kishk <ahmed.vet_2906@vet.kfs.edu.eg>
2024-07-03 20:10:19 +0300

    optimized search

--- app/src/main/java/com/example/movielist/ui/viewmodels/SearchFragmentViewModel.kt
+++ app/src/main/java/com/example/movielist/ui/viewmodels/SearchFragmentViewModel.kt
@@ -2,9 +2,13 @@ package com.example.movielist.ui.viewmodels
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.example.movielist.ThisApp
 import com.example.movielist.data.models.MovieCategory
 import com.example.movielist.data.models.Keyword
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 
 class SearchFragmentViewModel : BaseListViewModel(MovieCategory.UNCATEGORIZED) {
 
@@ -13,7 +17,7 @@ class SearchFragmentViewModel : BaseListViewModel(MovieCategory.UNCATEGORIZED) {
 
     /**
      * should call clearItemsList() and notify remove changes to recycler view before calling this
-      */
+     */
     fun submitSearch(text: String) {
         if (mIsRequestingNewSearch.value == true || text.isEmpty())
             return
@@ -26,9 +30,14 @@ class SearchFragmentViewModel : BaseListViewModel(MovieCategory.UNCATEGORIZED) {
             mIsRequestingNewSearch.postValue(true)
             // remove noResults state if it was true to indicate we are trying to get results
             mNoResults.postValue(false)
-            ThisApp.moviesRepository.findKeywords(text).let {
-                reload(it)
+
+            val requests = mutableListOf<Deferred<List<Keyword>>>()
+            requests.add(viewModelScope.async { ThisApp.moviesRepository.findKeywords(text) })
+            text.split(" ").take(5).forEach {
+                requests.add(viewModelScope.async { ThisApp.moviesRepository.findKeywords(it) })
             }
+            reload(requests.awaitAll().flatten())
+
             mIsRequestingNewSearch.postValue(false)
         }
     }
